080a9b6717246fbac7606dda487c297fde4aee2d
Yonatan Bensimon <106496877+yonatanbensimon@users.noreply.github.com>
2024-11-25 16:05:20 +0000

    Fixed aspect ratio problem

--- JS/map.js
+++ JS/map.js
@@ -1,36 +1,50 @@
-const openPopupButtons = document.querySelectorAll('[data-modal-target]');
-const closePopupButtons = document.querySelectorAll('[data-close-button]');
-const overlay = document.getElementById('overlay');
+$(document).ready(function() {
+  const $img = $('#map');
 
-openPopupButtons.forEach(area => {
-  area.addEventListener('click',() => {
-    const popup = document.querySelector(area.dataset.modalTarget)
-    openModal(popup)
-  })
-});
-
-closePopupButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const popup = button.closest('.popup')
-    closeModal(popup)
-  })
-});
-
-overlay.addEventListener('click',() => {
-  const popups = document.querySelectorAll('.popup.active')
-  popups.forEach((popup) => {
-    closeModal(popup)
+  // Wait for the image to fully load
+  $img.on('load', function() {
+    // Initialize the image map highlighting
+    $(this).rwdImageMaps();
+    $('.mapper').maphilight().parent().addClass('center-map');
+  }).each(function() {
+    // For cached images
+    if(this.complete) $(this).trigger('load');
   });
-})
 
-function openModal(popup) {
-  if (popup == null) return
-  popup.classList.add('active')
-  overlay.classList.add('active')
-}
+  const openPopupButtons = document.querySelectorAll('[data-modal-target]');
+  const closePopupButtons = document.querySelectorAll('[data-close-button]');
+  const overlay = document.getElementById('overlay');
 
-function closeModal(popup) {
-  if (popup == null) return
-  popup.classList.remove('active')
-  overlay.classList.remove('active')
-}
+  openPopupButtons.forEach(area => {
+    area.addEventListener('click',() => {
+      const popup = document.querySelector(area.dataset.modalTarget)
+      openModal(popup)
+    })
+  });
+
+  closePopupButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const popup = button.closest('.popup')
+      closeModal(popup)
+    })
+  });
+
+  overlay.addEventListener('click',() => {
+    const popups = document.querySelectorAll('.popup.active')
+    popups.forEach((popup) => {
+      closeModal(popup)
+    });
+  });
+
+  function openModal(popup) {
+    if (popup == null) return
+    popup.classList.add('active')
+    overlay.classList.add('active')
+  }
+
+  function closeModal(popup) {
+    if (popup == null) return
+    popup.classList.remove('active')
+    overlay.classList.remove('active')
+  }
+});
